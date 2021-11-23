c04bdf1565be07b11710acff39012f035a99734f
HA <>
2021-11-23 22:28:00 +0800

    add registry contract

---
+++ contracts/SkyrimRegistry.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.8.0;
+
+import "./library/Ownable.sol";
+
+contract SkyrimRegistry is Ownable {
+    mapping(address => bytes32) handlers;
+
+    bytes32 constant DEPRECATED = bytes10(0x64657072656361746564);
+
+    function register(address registration, bytes32 info) external onlyOwner {
+        require(registration != address(0), "zero address");
+        require(handlers[registration] == bytes32(0), "registered");
+        handlers[registration] = info;
+    }
+
+    function unregister(address registration) external onlyOwner {
+        require(registration != address(0), "zero address");
+        require(handlers[registration] != bytes32(0), "no registration");
+        require(handlers[registration] != DEPRECATED, "unregistered");
+        handlers[registration] = DEPRECATED;
+    }
+
+    function isValid(address handler) external view returns (bool result) {
+        if (handlers[handler] == 0 || handlers[handler] == DEPRECATED)
+            return false;
+        else
+            return true;
+    }
+
+    function getInfo(address handler) external view returns (bytes32 info) {
+        return handlers[handler];
+    }
+}
