7eb419e15d3a859134f505dff6379bd463be8142
HA <>
2021-12-03 23:21:22 +0800

    refactor admin

--- contracts/SkyrimInvestVaultAdmin.sol
+++ contracts/SkyrimInvestVaultAdmin.sol
@@ -7,7 +7,7 @@ import "./SkyrimInvestVault.sol";
 import "./interface/ISeniorToken.sol";
 import "./interface/IJuniorToken.sol";
 import "./library/SafeMathRatio.sol";
-
+import "hardhat/console.sol";
 /**
  * @title Skyrim invest vault admin contract.
  */
@@ -17,9 +17,7 @@ contract SkyrimInvestVaultAdmin is SkyrimInvestVault {
     using SafeMathRatio for uint256;
 
     function setStartTime(uint256 newStartTime) external onlyOwner nonReentrant {
-        // Gets old start time.
         uint256 oldStartTime = startTime;
-        // Sets new start time.
         startTime = newStartTime;
 
         emit NewStartTime(oldStartTime, newStartTime);
@@ -29,7 +27,6 @@ contract SkyrimInvestVaultAdmin is SkyrimInvestVault {
         uint256 profit;
         uint256 loss;
         TokenType tokenType;
-        uint256 currentPeriod;
         uint256 oldShareTRARewardRate;
         uint256 oldInvestmentPricePerPeriod;
         uint256 lockedInvestment;
@@ -50,7 +47,7 @@ contract SkyrimInvestVaultAdmin is SkyrimInvestVault {
      * @notice Senior token details must be at the first place!!!
      * @dev Input profits and losses of the last investment, only for owner.
      *      eg: profits = [STProfit, JTProfit]
-     *      eg: losses = [STLoss, JTLoss]
+     *      eg: losses =  [STLoss, JTLoss]
      */
     function settleProfitsByOwner(
         uint256[] memory profits,
@@ -58,128 +55,78 @@ contract SkyrimInvestVaultAdmin is SkyrimInvestVault {
     ) external onlyOwner nonReentrant {
         require(
             profits.length == losses.length && profits.length == 2,
-            "settleProfitsByOwner: profits & losses must have 2 elements!"
+            "profits & losses must have 2 elements!"
         );
 
-        for (uint256 i = 0; i < profits.length; i++) {
+        // let's deal with the profits and losses of ST and then JT
+        for (uint8 i = 0; i < 2; i++) {
+            require(
+                profits[i] == 0 || losses[i] == 0,
+                "One of profit or loss must be zero!"
+            );
+
             SettleProfitsLocalVars memory _vars;
             _vars.profit = profits[i];
             _vars.loss = losses[i];
-            _vars.tokenType = i == 0
-                ? TokenType.isSeniorToken
-                : TokenType.isJuniorToken;
+            _vars.tokenType = i == 0 ? TokenType.isSeniorToken : TokenType.isJuniorToken;
+            _vars.oldShareTRARewardRate = TRAPricePerPeriod[_vars.tokenType];
 
-            TotalInvestmentSnapshot storage totalInvestment =
-                totalInvestmentsInfo[_vars.tokenType];
-
-            _vars.currentPeriod = currentPeriod;
-            _vars.burnedTimes = burnedTimes[_vars.tokenType];
-            if (1 == _vars.currentPeriod) {
-                // The first investing round starts.
-                totalInvestment.lockedInvestment = totalInvestment.pendingInvestment;
-                totalInvestment.pendingInvestment = 0;
-                TRAPricePerPeriod[_vars.tokenType][
-                    _vars.currentPeriod
-                ] = 0;
-                investmentPricePerPeriod[_vars.tokenType][
-                    _vars.currentPeriod
-                ] = BASE;
-            } else {
-                require(
-                    _vars.profit == 0 || _vars.loss == 0,
-                    "settleProfitsByOwner: One of profit or loss must be zero!"
-                );
-
-                _vars.lockedInvestment = totalInvestment.lockedInvestment;
-                _vars.oldUnlockedInvestment = totalInvestment.unlockedInvestment;
-                _vars.oldShareTRARewardRate = TRAPricePerPeriod[_vars.tokenType][_vars.currentPeriod.sub(1)];
-                _vars.oldInvestmentPricePerPeriod = investmentPricePerPeriod[_vars.tokenType][_vars.currentPeriod.sub(1)];
-                _vars.totalUnlockedInvestment = _vars.oldUnlockedInvestment.add(_vars.lockedInvestment);
-                // Update rewards of the invested senior token.
-                if (_vars.profit > 0) {
-                    totalTRARewards = totalTRARewards.add(_vars.profit);
-                    if (_vars.totalUnlockedInvestment == 0) {
-                        _vars.changedTRARewardRate = 0;
-                    } else {
-                        _vars.changedTRARewardRate = _vars.profit.rdiv(
-                            totalInvestment.totalShare
-                        );
-                    }
-
-                    if (i == 1) {
-                        _vars.threeDaysAgoSupplyRate = currentPeriod == 1 ? 0 : JTSupplyRatesPerPeriod[_vars.currentPeriod.sub(2)];
-                        _vars.oldJTSupplyRate = JTSupplyRatesPerPeriod[_vars.currentPeriod.sub(1)];
-                        _vars.currentJTSupplyRate = _vars.oldJTSupplyRate.add(_vars.changedTRARewardRate);
-                        _vars.newJTSupplyRate =  _vars.currentJTSupplyRate.add(_vars.oldJTSupplyRate).add(_vars.threeDaysAgoSupplyRate).div(3);
-                        _setJuniorTokenSupplyRateInternal(_vars.currentPeriod, _vars.newJTSupplyRate);
-                    }
-
-                    totalInvestment.unlockedInvestment = _vars.totalUnlockedInvestment;
-                    totalInvestment.lockedInvestment = totalInvestment.pendingInvestment;
-                    totalInvestment.pendingInvestment = 0;
-                    TRAPricePerPeriod[_vars.tokenType][
-                        _vars.currentPeriod
-                    ] = _vars.oldShareTRARewardRate.add(
-                        _vars.changedTRARewardRate
-                    );
-                    investmentPricePerPeriod[_vars.tokenType][
-                        _vars.currentPeriod
-                    ] = _vars.oldInvestmentPricePerPeriod;
+            TotalInvestmentSnapshot storage totalInvestment = totalInvestmentsInfo[_vars.tokenType];
+            _vars.totalUnlockedInvestment = totalInvestment.pendingInvestment;
+            console.log("totalUnlockedInvestment %s", _vars.totalUnlockedInvestment);
+            // Luckily we made money
+            if (_vars.profit > 0) {
+                totalTRARewards = totalTRARewards.add(_vars.profit);
+                if (_vars.totalUnlockedInvestment == 0) {
+                    _vars.changedTRARewardRate = 0;
                 } else {
-                    require(_vars.loss <= _vars.totalUnlockedInvestment, "settleProfitsByOwner: Too much to lose!");
-                    // Loss senior token, decrease the exchange ratio of invested Token and invested Share.
-                    if (_vars.loss != 0 && _vars.loss == _vars.totalUnlockedInvestment) {
-                        // All invested token has lost.
-                        _vars.finalInvestmentPrice = BASE;
-                        TRAPricePerPeriod[_vars.tokenType][
-                            _vars.currentPeriod
-                        ] = 0;
+                    _vars.changedTRARewardRate = _vars.profit.rdiv(totalInvestment.totalShare);
+                }
+                console.log("changedTRARewardRate %s", _vars.changedTRARewardRate);
 
-                        burnedTRARate[_vars.tokenType][
-                            _vars.burnedTimes
-                        ] = _vars.oldShareTRARewardRate;
-                        burnedTimes[_vars.tokenType] = _vars.burnedTimes.add(1);
+                totalInvestment.pendingInvestment = 0;
+                totalInvestment.unlockedInvestment = _vars.totalUnlockedInvestment;
+
+                TRAPricePerPeriod[_vars.tokenType] = _vars.changedTRARewardRate;
+                // investmentPricePerPeriod[_vars.tokenType] = _vars.oldInvestmentPricePerPeriod;
+            } else {
+                // I'm sorry we lost out
+                require(_vars.loss <= _vars.totalUnlockedInvestment, "Too much to lose!");
+                // Loss senior token, decrease the exchange ratio of invested Token and invested Share.
+                if (_vars.loss != 0 && _vars.loss == _vars.totalUnlockedInvestment) {
+                    // All invested token has lost.
+                    _vars.finalInvestmentPrice = BASE;
+                    TRAPricePerPeriod[_vars.tokenType] = 0;
+                } else {
+                    if (_vars.loss != 0 && _vars.totalUnlockedInvestment != 0) {
+                        _vars.changedInvestmentPrice = _vars.loss.rdivup(_vars.totalUnlockedInvestment);
+                        _vars.finalInvestmentPrice = _vars.oldInvestmentPricePerPeriod.rmul(BASE.sub(_vars.changedInvestmentPrice));
                     } else {
-                        if (_vars.loss != 0 && _vars.totalUnlockedInvestment != 0) {
-                            _vars.changedInvestmentPrice = _vars.loss.rdivup(
-                                _vars.totalUnlockedInvestment
-                            );
-                            _vars.finalInvestmentPrice = _vars
-                                .oldInvestmentPricePerPeriod
-                                .rmul(BASE.sub(_vars.changedInvestmentPrice));
-                        } else {
-                            _vars.finalInvestmentPrice = _vars.oldInvestmentPricePerPeriod;
-                        }
-                        TRAPricePerPeriod[_vars.tokenType][
-                            _vars.currentPeriod
-                        ] = _vars.oldShareTRARewardRate;
+                        _vars.finalInvestmentPrice = _vars.oldInvestmentPricePerPeriod;
                     }
-                    totalInvestment.unlockedInvestment = _vars
-                        .totalUnlockedInvestment.sub(_vars.loss);
-                    totalInvestment.lockedInvestment = totalInvestment.pendingInvestment;
+                    TRAPricePerPeriod[_vars.tokenType] = _vars.oldShareTRARewardRate;
+                }
 
-                    totalInvestment.pendingInvestment = 0;
+                totalInvestment.pendingInvestment = 0;
+                totalInvestment.unlockedInvestment = _vars.totalUnlockedInvestment.sub(_vars.loss);
 
-                    investmentPricePerPeriod[_vars.tokenType][
-                        _vars.currentPeriod
-                    ] = _vars.finalInvestmentPrice;
-                    if (_vars.loss > 0) {
-                        if (_vars.tokenType == TokenType.isSeniorToken) {
-                            STToken.vaultBurnLoss(_vars.loss);
-                        } else if (_vars.tokenType == TokenType.isJuniorToken) {
-                            JTToken.vaultBurnLoss(_vars.loss);
-                        }
+                // investmentPricePerPeriod[_vars.tokenType] = _vars.finalInvestmentPrice;
+                if (_vars.loss > 0) {
+                    if (_vars.tokenType == TokenType.isSeniorToken) {
+                        STToken.vaultBurnLoss(_vars.loss);
+                    } else if (_vars.tokenType == TokenType.isJuniorToken) {
+                        JTToken.vaultBurnLoss(_vars.loss);
                     }
                 }
             }
-            emit SettleProfits(_vars.currentPeriod, _vars.tokenType, _vars.profit, _vars.loss);
+
+            emit SettleProfits(_vars.tokenType, _vars.profit, _vars.loss);
         }
-        currentPeriod = currentPeriod.add(1);
+        currentState = PeriodState.End;
     }
 
     struct InvestByOwnerLocalVars {
         TokenType tokenType;
-        uint256 currentPeriod;
         uint256 currentInvestmentPrice;
         uint256 maxInvestmentAmount;
         uint256 expectWithdrawAmount;
@@ -198,21 +145,10 @@ contract SkyrimInvestVaultAdmin is SkyrimInvestVault {
             InvestByOwnerLocalVars memory _vars;
             _vars.expectWithdrawAmount = investmentAmounts[i];
             if (_vars.expectWithdrawAmount != 0) {
-                _vars.tokenType = i == 0
-                    ? TokenType.isSeniorToken
-                    : TokenType.isJuniorToken;
-                _vars.currentPeriod = currentPeriod;
-
-                TotalInvestmentSnapshot storage totalInvestment =
-                    totalInvestmentsInfo[_vars.tokenType];
-
-                _vars.currentInvestmentPrice = investmentPricePerPeriod[
-                    _vars.tokenType
-                ][_vars.currentPeriod.sub(1)];
-
-                _vars.maxInvestmentAmount = totalInvestment.unlockedInvestment.add(
-                    totalInvestment.lockedInvestment
-                );
+                _vars.tokenType = i == 0 ? TokenType.isSeniorToken : TokenType.isJuniorToken;
+                _vars.currentInvestmentPrice = investmentPricePerPeriod[_vars.tokenType];
+                TotalInvestmentSnapshot storage totalInvestment = totalInvestmentsInfo[_vars.tokenType];
+                _vars.maxInvestmentAmount = totalInvestment.pendingInvestment;
                 require(
                     _vars.expectWithdrawAmount <= _vars.maxInvestmentAmount,
                     "investByOwner: Too much to invest!"
@@ -229,7 +165,7 @@ contract SkyrimInvestVaultAdmin is SkyrimInvestVault {
 
                 investedToken.safeTransfer(msg.sender, _vars.expectWithdrawAmount);
 
-                emit InvestByOwner(_vars.currentPeriod, _vars.tokenType, _vars.expectWithdrawAmount);
+                emit InvestByOwner(_vars.tokenType, _vars.expectWithdrawAmount);
             }
         }
     }
@@ -264,24 +200,28 @@ contract SkyrimInvestVaultAdmin is SkyrimInvestVault {
     /**
      * @dev Set junior token supply rate per locked time.
      */
-    function _setJuniorTokenSupplyRateInternal(uint256 period, uint256 newSupplyRate) internal {
-        uint256 oldSupplyRate = JTSupplyRatesPerPeriod[period];
-
-        JTSupplyRatesPerPeriod[period] = newSupplyRate;
-
+    function _setJuniorTokenSupplyRateInternal(uint256 newSupplyRate) internal {
+        uint256 oldSupplyRate = JTSupplyRatesPerPeriod;
+        JTSupplyRatesPerPeriod = newSupplyRate;
         emit NewSupplyRate(TokenType.isJuniorToken, oldSupplyRate, newSupplyRate);
     }
 
     /**
      * @dev Set junior token supply rate, only for owner.
      */
-    function setJuniorTokenSupplyRate(uint256 period, uint256 newSupplyRatePerLockTime)
+    function setJuniorTokenSupplyRate(uint256 newSupplyRatePerLockTime)
         external
         onlyOwner
     {
-        _setJuniorTokenSupplyRateInternal(period, newSupplyRatePerLockTime);
+        _setJuniorTokenSupplyRateInternal(newSupplyRatePerLockTime);
     }
 
+    /**
+     * @dev Update Peroid State
+     */
+    function setPeroidState(PeriodState _targetState) external onlyOwner nonReentrant {
+        currentState = _targetState;
+    }
 
     /**
      * @dev Get current senior token supply rate.
@@ -293,7 +233,7 @@ contract SkyrimInvestVaultAdmin is SkyrimInvestVault {
     /**
      * @dev Get current junior token supply rate.
      */
-    function getCurrentJTSupplyRate() external view returns (uint256 JTSupplyRate) {
-        JTSupplyRate = JTSupplyRatesPerPeriod[currentPeriod];
+    function getCurrentJTSupplyRate() external view returns (uint256) {
+        return JTSupplyRatesPerPeriod;
     }
 }
