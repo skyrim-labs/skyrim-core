e884afb0ac8df66a31e280cd2b4a934af3aa411a
HA <>
2022-04-19 20:34:37 +0800

    feat: add busd test token

---
+++ contracts/mock/BUSD.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.6;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+
+contract BUSD is ERC20Upgradeable {
+    constructor() {
+        __ERC20_init("BUSD Stable Coin", "BUSD");
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
