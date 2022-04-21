d8e439b955a5b987e7ff5115c1b677fcf4268ee3
HA <>
2022-04-21 15:56:57 +0800

    add config file

---
+++ config/contractAddress.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "bsc_test": {
+        "BUSD": "0xe22d0BDd5769722E8C181b4b8BDe0433E45C7D9A",
+        "TRA": "0x60498990e6f85D8ba740f3AC97F1D07f75a4d8E8",
+        "STToken": "0x6cA916EFfA7f5309F8706341851906b18D8Be5cc",
+        "JTToken": "0xa4436F1fC59BD342855f1ccD9418A1E4e4CF4214",
+        "vault": "",
+        "STSupplyRate": "1.1", // APY 10%
+        // "lockedTime": 60*60*24,
+        "lockedTime": 5 * 60,    // 5 minutes, only for test
+    },
+}
